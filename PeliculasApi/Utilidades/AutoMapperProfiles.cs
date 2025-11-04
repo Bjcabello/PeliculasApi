90b949adcc0d8e8d9c805a9086b8f42b0da7b3dd
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2025-11-04 17:43:25 -0500

    Obteniendo Películas

--- PeliculasApi/Utilidades/AutoMapperProfiles.cs
+++ PeliculasApi/Utilidades/AutoMapperProfiles.cs
@@ -22,7 +22,29 @@ namespace PeliculasApi.Utilidades
                 .ForMember(x => x.PeliculaGeneros, dto => dto.MapFrom(p => p.GenerosIds!.Select(id => new PeliculaGenero { GeneroId = id })))
                 .ForMember(x => x.PeliculaCines, dto => dto.MapFrom(p => p.CinesIds!.Select(id => new PeliculaCine { CineId = id })))
                 .ForMember(p => p.PeliculaActores, dto => dto.MapFrom(p => p.Actores!.Select(actor => new PeliculaActor { ActorId = actor.Id, Personaje = actor.Personaje })));
+            
             CreateMap<Pelicula, PeliculaDTO>();
+
+            CreateMap<Pelicula, PeliculaDetallesDTO>()
+                .ForMember(p => p.Generos, entidad => entidad.MapFrom(p => p.PeliculaGeneros))
+                .ForMember(p => p.Cines, entidad => entidad.MapFrom(p => p.PeliculaCines))
+                .ForMember(p => p.Actores, entidad => entidad.MapFrom(p => p.PeliculaActores.OrderBy(o => o.Orden)));
+
+            CreateMap<PeliculaGenero, GeneroDTO>()
+                .ForMember(g => g.Id, pg => pg.MapFrom(p => p.GeneroId))
+                .ForMember(g => g.Nombre, pg => pg.MapFrom(p => p.Genero.Nombre));
+
+            CreateMap<PeliculaCine, CineDTO>()
+                .ForMember(g => g.Id, pc => pc.MapFrom(p => p.CineId))
+                .ForMember(g => g.Nombre, pc => pc.MapFrom(p => p.Cine.Nombre))
+                .ForMember(g => g.Latitud, pc => pc.MapFrom(p => p.Cine.Ubicacion.Y))
+                .ForMember(g => g.Longitud, pc => pc.MapFrom(p => p.Cine.Ubicacion.X));
+
+            CreateMap<PeliculaActor, PeliculaActorDTO>()
+                .ForMember(dto => dto.Id, entidad => entidad.MapFrom(p => p.ActorId))
+                .ForMember(dto => dto.Nombre, entidad => entidad.MapFrom(p => p.Actor.Nombre))
+                .ForMember(dto => dto.Foto, entidad => entidad.MapFrom(p => p.Actor.Foto));
+
         }
 
         private void ConfigurarMapeoCines(GeometryFactory geometryFactory)
